1dee486d778014491a8279de4711c0370fdee798
David Winderl <davidwinderl@Davids-MacBook-Pro.local>
2020-06-16 09:42:23 +0200

    added first basics of MW

--- algorithms/solver/SMP/mcvitie.py
+++ algorithms/solver/SMP/mcvitie.py
@@ -1,16 +1,19 @@
+from copy import deepcopy
+
 import numpy as np
 
 
 class MCVITIE:
     def __init__(self, matching):
         self.matching = matching
+
         self.unchanged = [True for _ in range(matching.size)]
-        self.malecounter = [1 for _ in range(matching.size)]
+
         self.solutions = []
+
         self.malechoice = np.zeros((matching.size, matching.size), dtype=int)
         self.femalechoice = np.zeros((matching.size, matching.size), dtype=int)
         self.fc = np.zeros((matching.size, matching.size + 1))
-        self.success = False
 
         for idx, m in enumerate(matching.males):
             self.malechoice[idx] = [self.matching.females.index(w) for w, idx in
@@ -26,49 +29,51 @@ class MCVITIE:
             self.fc[i][0] = self.matching.size + 1
 
     def solve(self):
+        malec = [1 for _ in range(self.matching.size)]
         marriage = [0 for _ in range(self.matching.size)]
         for i in range(1, self.matching.size + 1):
-            self.proposal(i, marriage)
+            self.proposal(i, malec, marriage)
         self.store_stable_marrigae(marriage)
         for i in range(1, self.matching.size):
-            self.breakmarriage(marriage, i)
+            self.breakmarriage(malec, marriage, i)
         return self.solutions
 
-    def breakmarriage(self, marriage: list, i: int):
-        print(self.malecounter[i - 1])
-        marriage[self.malechoice[i - 1][self.malecounter[i - 1] - 1]] = -i
-        self.proposal(i, marriage)
-        if not self.success:
+    def breakmarriage(self, malec: list, marriage: list, i: int):
+        marriage[self.malechoice[i - 1][malec[i - 1] - 2]] = -i
+        sucess = self.proposal(i, malec, marriage)
+        if not sucess:  # GOTO EXIT
             self.unchanged[i - 1] = False
             return
         self.store_stable_marrigae(marriage)
-        for j in range(i, self.matching.size - 1):
-            self.breakmarriage(marriage, j)
-        for j in range(i + 1, self.matching.size - 1):
-            self.unchanged[j] = True
+        malec_ = deepcopy(malec)
+        for j in range(i, self.matching.size):
+            self.breakmarriage(malec_, marriage, j)
 
-    def proposal(self, i: int, marriage: list):
+        for j in range(i + 1, self.matching.size):
+            self.unchanged[j - 1] = True
+
+    def proposal(self, i: int, malec: list, marriage: list):
         if i < 0:
-            self.success = True
-        elif i == 0 or self.malecounter[i - 1] == self.matching.size + 1 or not self.unchanged[i - 1]:
-            self.success = False
-        else:
-            j = self.malecounter[i - 1]
-            self.malecounter[i - 1] = j + 1
-            self.refusal(i, self.malechoice[i - 1][j - 1] + 1, marriage)
+            return True
+        elif i == 0 or malec[i - 1] == self.matching.size + 1 or (not self.unchanged[i - 1]):
+            return False
 
-    def refusal(self, i: int, j: int, marriage: list):
-        if self.fc[j - 1][abs(marriage[j - 1])] > self.fc[j - 1][i]:  # todo abs here
+        else:
+            j = malec[i - 1]
+            malec[i - 1] = j + 1
+            return self.refusal(i, self.malechoice[i - 1][j - 1] + 1, malec, marriage)
+
+    def refusal(self, i: int, j: int, malec: list, marriage: list):
+        if self.fc[j - 1][abs(marriage[j - 1])] > self.fc[j - 1][i]:
             k = marriage[j - 1]
             marriage[j - 1] = i
-            self.proposal(k, marriage)
+            return self.proposal(k, malec, marriage)
         else:
-            self.proposal(i, marriage)
+            return self.proposal(i, malec, marriage)
 
     def store_stable_marrigae(self, marriage):
         match = {}
         for index, i in enumerate(marriage):
-            i = abs(i)
             m = self.matching.males[i - 1]
             w = self.matching.females[index]
             match[m] = w
--- computations/test.py
+++ computations/test.py
@@ -1,20 +1,57 @@
 import time
 import gc
 
-from algorithms.create_templates import create_smp_instance
+from algorithms.create_templates import create_smp_instance, create_and_save_smp
 from algorithms.solution import Solution
 from algorithms.solver.SMP.backtrack_smp import BACKTRACK_SMP
 from algorithms.solver.SMP.mcvitie import MCVITIE
+from algorithms.solver.SMP.std_smp import StandardSMP
 from algorithms.solver.SMTI.qubo_smti import QUBO_SMTI
+from algorithms.storage import get_smp
 
 
 def test_main():
-    run_size(4)
+    for _ in range(100):
+        run_size(4)
+
+
+def check_soltution(to_check, ground_truth):
+    for solution in to_check:
+        correct_solution = False
+        for solution_b in ground_truth:
+            tmp = True
+            for m, w in solution.items():
+                if not solution_b[m] == w:
+                    tmp = False
+                    break
+            if tmp:
+                correct_solution = True
+        assert correct_solution
 
 
 def run_size(size):
+    # uid = 101
+    # matching = create_and_save_smp(uid, size)
+    # matching = get_smp(uid, size)
     matching = create_smp_instance(size)
+
     solutions_b = (BACKTRACK_SMP(matching).solve())
+    solutions_b = list(map(lambda x: x.solution_m, solutions_b))
+
     solutions_mv = MCVITIE(matching).solve()
-    print(list(map(lambda x: Solution(matching, x).is_stable(), solutions_mv)))
-    print(len(solutions_b), len(solutions_mv))
+    solutions_mv = list(filter(lambda x: len(x) == size, solutions_mv))
+    print(solutions_b)
+    print(solutions_mv)
+    print("B, male: ", list(map(lambda x: Solution(matching, x).get_male_loss(), solutions_b)))
+    print("B, female: ", list(map(lambda x: Solution(matching, x).get_female_loss(), solutions_b)))
+    print("MV, male: ", list(map(lambda x: Solution(matching, x).get_male_loss(), solutions_mv)))
+    print("MV, female: ", list(map(lambda x: Solution(matching, x).get_female_loss(), solutions_mv)))
+    print()
+    assert all(list(map(lambda x: Solution(matching, x).is_stable()[0], solutions_mv)))
+    assert all(list(map(lambda x: Solution(matching, x).is_stable()[0], solutions_b)))
+
+    # print(StandardSMP(matching).solve().solution_m)
+    # print(solutions_mv[0])
+    assert (len(solutions_mv) == len(solutions_b))
+    # check_soltution(solutions_b, solutions_mv)
+    # check_soltution(solutions_mv, solutions_b)
